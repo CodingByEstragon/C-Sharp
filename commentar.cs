b3425705fed44678a66432e505abb465afb0bdbb
Estragon <124503629+CodingByEstragon@users.noreply.github.com>
2023-07-03 19:48:36 +0200

    Kommentar - commentar

---
+++ commentar.cs
@@ -0,0 +1,22 @@
+using Daten;
+
+namespace Ablauf
+{
+    class Program
+    {
+        // einzeiliger Kommentar
+
+        /*
+         mehrzeiliger
+        Kommentar
+         */
+
+
+        /// <summary>
+        /// Dokumentationskommentar
+        /// </summary>
+        static void Main()
+        {
+        }
+    }
+}
