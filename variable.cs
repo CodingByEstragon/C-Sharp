299c4cb26333792ad8781c5fe515b4880b858006
Estragon <124503629+CodingByEstragon@users.noreply.github.com>
2023-07-03 20:05:48 +0200

    Variablen - variables

---
+++ variable.cs
@@ -0,0 +1,22 @@
+using Daten;
+
+namespace Ablauf
+{
+    class Program
+    {
+        static void Main()
+        {
+            // initialisieren
+            int zahl = 2;
+
+            // deklarieren
+            int zahl2;
+
+            // zuweisen
+            zahl2 = 12;
+
+            // Ausgabe
+            Console.Write(zahl + zahl2);
+        }
+    }
+}
