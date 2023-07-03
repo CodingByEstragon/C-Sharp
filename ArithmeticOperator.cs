3178f99ee9ec369a4bfc7b4ee426479632551d1f
Estragon <124503629+CodingByEstragon@users.noreply.github.com>
2023-07-03 20:46:23 +0200

    Arithmetic operator

---
+++ ArithmeticOperator.cs
@@ -0,0 +1,23 @@
+namespace Ablauf
+{
+    class Program
+    {
+        static void Main()
+        {
+            int number1 = 7;
+            int number2 = 12;
+            Console.WriteLine(number1);
+            Console.WriteLine(number2);
+
+            number1 = 9;
+            number2 = 15;
+            Console.WriteLine(number1 + number2);
+            Console.WriteLine(number1 - number2);
+
+            number1 = 12;
+            number2 = 3;
+            Console.WriteLine(number1 * number2);
+            Console.WriteLine(number1 / number2);
+        }
+    }
+}
