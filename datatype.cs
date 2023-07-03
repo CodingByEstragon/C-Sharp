b13e070a260f0a068b82d6ec5f8a2bcda901bee7
Estragon <124503629+CodingByEstragon@users.noreply.github.com>
2023-07-03 20:09:08 +0200

    Datentypen - datatypes

---
+++ datatype.cs
@@ -0,0 +1,36 @@
+using Daten;
+
+namespace Ablauf
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Integer - ganze Zahl
+            int i = 7;
+
+            // double - Gleitpunktzahl
+            double d = 2.7;
+
+            // float - Gleitpunktzahl
+            float f = 4.2f;
+
+            // string - Zeichenkette
+            string str = "Das ist ein Text";
+
+            // character - einzelne Zeichen
+            char c = 'a';
+
+            // boolean - Wahrheitswert
+            bool b = true;
+
+            Console.WriteLine(i);
+            Console.WriteLine(d);
+            Console.WriteLine(f);
+            Console.WriteLine(str);
+            Console.WriteLine(c);
+            Console.WriteLine(b);
+
+        }
+    }
+}
