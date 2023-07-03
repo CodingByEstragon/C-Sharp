b61541a3fcf8181d69f5523d8a83b0f270add4ff
Estragon <124503629+CodingByEstragon@users.noreply.github.com>
2023-07-03 20:49:43 +0200

    modulo operator

---
+++ ModuloOperator.cs
@@ -0,0 +1,16 @@
+namespace Ablauf
+{
+    class Program
+    {
+        static void Main()
+        {
+            // 23 / 4 = 5,75  ---> 5
+            int number1 = 22 / 4;
+            Console.WriteLine(number1);
+
+            // 15 % 4 = 3 ---> 12 Rest 3
+            int number2 = 15 % 4;
+            Console.WriteLine(number2);
+        }
+    }
+}
