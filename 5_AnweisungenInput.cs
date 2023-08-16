0d3f79c1ae0cbfe108d5cc089606484e6776fec8
Estragon <124503629+CodingByEstragon@users.noreply.github.com>
2023-08-16 15:14:54 +0200

    5_AnweisungenInput.cs

--- 5_AnweisungenInput.cs
+++ 5_AnweisungenInput.cs
@@ -1,32 +1,28 @@
-using Daten;
-
 namespace Ablauf
 {
     class Program
     {
         static void Main()
         {
-            // 1.example
-            Console.WriteLine("Gib mir deinen Vornamen ein: ");
-            string vorname = Console.ReadLine();
-            Console.WriteLine(vorname);
+            //Console.WriteLine("Gib einen Autohersteller an: ");
+            //string hersteller = Console.ReadLine();
+            //Console.WriteLine(hersteller);
 
-            Console.WriteLine("Gib mir deinen Vornamen ein: ");
-            string nachname = Console.ReadLine();
-            Console.WriteLine(nachname);
+            //Console.WriteLine("Gib das Automdell an: ");
+            //string modell = Console.ReadLine();
+            //Console.WriteLine(modell);
 
-            Console.WriteLine(vorname + " " + nachname);
+            //Console.WriteLine(hersteller + " " + modell);
 
             Console.WriteLine();
 
-            // 2.example
-            Console.WriteLine("Gib eine Zahl ein: ");
+            Console.WriteLine("Gib die erste Zahl ein: ");
             int zahl1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(zahl1);
 
-            Console.WriteLine("Gib eine zweite Zahl ein: ");
+            Console.WriteLine("Gib die zweite Zahl ein: ");
             int zahl2 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("result: " + (zahl1 + zahl2));
+            Console.WriteLine(zahl2);
         }
     }
 }
