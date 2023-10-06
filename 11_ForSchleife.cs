7f259b5af49ba0379406f24e28813f6eca0380fe
Estragon <124503629+CodingByEstragon@users.noreply.github.com>
2023-10-06 20:40:16 +0200

    11_ForSchleife.cs

--- 11_ForSchleife.cs
+++ 11_ForSchleife.cs
@@ -4,11 +4,34 @@ namespace Ablauf
     {
         static void Main()
         {
-            Console.WriteLine("Gib eine Zahl ein: ");
-            int zahl = Convert.ToInt32(Console.ReadLine());
-            
-            for(int i = 0; i < zahl; i++) {
-                Console.WriteLine(i);
+            // Grundgerüst einer For Schleife
+            //for (Initialsierung; Bedingung; Variablenveränderung)
+            //{
+            //    Console.WriteLine();
+            //}
+
+            // einfache For Schleife
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            // Endlosschleife
+            //for (int zahl = 0; zahl < 10;)
+            //{
+            //    Console.WriteLine(zahl);
+            //}
+
+            int zwsumme = 0;
+
+            for(int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Gib eine Zahl ein: ");
+                int zahl = Convert.ToInt32(Console.ReadLine());
+
+                //zwsumme += zahl;
+                zwsumme = zwsumme + zahl;
+                Console.WriteLine("Summe: " + zwsumme);
             }
         }
     }
