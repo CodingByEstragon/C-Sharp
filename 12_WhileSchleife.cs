1723fd091733d38c8a8101e6f3d8b54de6a8a01a
Estragon <124503629+CodingByEstragon@users.noreply.github.com>
2023-10-14 16:04:00 +0200

    12_WhileSchleife.cs

--- 12_WhileSchleife.cs
+++ 12_WhileSchleife.cs
@@ -4,13 +4,43 @@ namespace Ablauf
     {
         static void Main()
         {
-            Console.WriteLine("Gib ein Zahl ein: ");
-            int counter = Convert.ToInt32(Console.ReadLine());
+            // Grundgerüst einer while Schleife
+            //Intialisierung;
+            //while (Bedingung)
+            //{
+            //    Ausgabe;
+            //    Variablenveränderung;
+            //}
 
-            int num = 0;
-            while (num < counter) {
-                Console.WriteLine(num);
-                num++;
+            // Endlosschleife
+            //int anzahl = 5;
+            //int i = 0;
+            //while (i < anzahl)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            // einfach while Schleife
+            //int anzahl = 5;
+            //int i = 0;
+            //while (i < anzahl)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+
+            // Anwendungsbeispiel for Schleife in while Schleife umschreiben
+            int zwsumme = 0;
+            int i = 0;
+            while (i < 4)
+            {
+                Console.WriteLine("Gib eine Zahl ein: ");
+                int zahl = Convert.ToInt32(Console.ReadLine());
+
+                //zwsumme += zahl;
+                zwsumme = zwsumme + zahl;
+                Console.WriteLine("Summe: " + zwsumme);
+                i++;
             }
         }
     }
