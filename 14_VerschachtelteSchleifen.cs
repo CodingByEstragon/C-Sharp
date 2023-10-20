08e891ab82c8c5012dc7f76faef05417f48c8a97
Estragon <124503629+CodingByEstragon@users.noreply.github.com>
2023-10-20 18:59:21 +0200

    14_VerschachtelteSchleifen.cs

--- 14_VerschachtelteSchleifen.cs
+++ 14_VerschachtelteSchleifen.cs
@@ -2,21 +2,42 @@ namespace Ablauf
 {
     class Program
     {
-        static void Main() {
-            Console.WriteLine("Gib eine Zahl ein: ");
+        static void Main()
+        {
+            // Verschachtelte Schleifen - Rechteck
+            //Console.WriteLine("Gib die Höhe ein: ");
+            //int hoehe = Convert.ToInt32 (Console.ReadLine());
+
+            //Console.WriteLine("Gib die Breite ein: ");
+            //int breite = Convert.ToInt32 (Console.ReadLine());
+
+            //for (int i = 0; i < hoehe; i++)
+            //{
+            //    int j = 0;
+            //    while (j < breite) 
+            //    {
+            //        Console.Write("*");
+            //        j++;
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            // Verschachtelte Schleifen - Treppenform
+            Console.WriteLine("Gib die Höhe ein: ");
             int hoehe = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Gib eine zweite Zahl ein: ");
-            int breite = Convert.ToInt32(Console.ReadLine());
-
-            for(int i = 0; i < hoehe; i++)
+            for (int i = 0; i < hoehe; i++)
             {
-                for(int j = 0; j < breite; j++)
+                int j = 0;
+                while (j < i + 1)
                 {
                     Console.Write("*");
+                    j++;
                 }
                 Console.WriteLine();
             }
+
         }
     }
 }
