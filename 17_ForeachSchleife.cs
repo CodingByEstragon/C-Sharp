39ccb0a6ef1026b12690dffd3d3355474a2233b1
Estragon <124503629+CodingByEstragon@users.noreply.github.com>
2023-11-17 17:33:50 +0100

    17_ForeachSchleife.cs

--- 17_ForeachSchleife.cs
+++ 17_ForeachSchleife.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Reflection;
 
 namespace Ablauf
 {
@@ -7,7 +6,30 @@ namespace Ablauf
     {
         static void Main()
         {
+            // foreach Schleife
+            //Datentyp[] Arrayname = new Datentyp[] { wert1, wert2, wert3 };
+            //foreach(Datentyp Variablenname in Arrayname)
+            //{
+            //    Console.WriteLine(Variablenname);
+            //}
 
+            // zwei einfache Beispiele
+            string[] firstname = new string[] { "Laura", "Mike", "Sarah" };
+            foreach (string name in firstname)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("-------------------------");
+
+            int[] zahl = new int[3];
+            zahl[0] = 12;
+            zahl[1] = 13;
+            zahl[2] = 14;
+            foreach(int zahlen in zahl)
+            {
+                Console.WriteLine(zahlen);
+            }
         }
     }
 }
