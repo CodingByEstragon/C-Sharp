767f5695351bda6997403cfd70d0a016d20c35cd
Estragon <124503629+CodingByEstragon@users.noreply.github.com>
2023-12-06 20:36:55 +0100

    20_Funktionen.cs

--- 20_Funktionen.cs
+++ 20_Funktionen.cs
@@ -4,7 +4,37 @@ namespace Ablauf
     {
         static void Main()
         {
+            // Methoden
+            Greeting("Jack");
+            double ergebnis = Rechteck(2.5, 3.7);
+            Console.WriteLine("Flächeninhalt: " + ergebnis + "cm");
 
+            int alter = 17;
+            if (Age(alter))
+            {
+                Console.WriteLine("Du bist erwachsen");
+            }
+            else
+            {
+                Console.WriteLine("Du bist noch zu jung");
+            }
+        }
+
+        // Methode ohne Rückgabewert
+        static void Greeting(string name)
+        {
+            Console.WriteLine("Hallo " + name);
+        }
+
+        // Methode mit Rückgabewert
+        static double Rechteck(double breite, double hoehe)
+        {
+            return breite * hoehe;
+        }
+
+        static bool Age(int alter)
+        {
+            return alter > 17;
         }
     }
 }
