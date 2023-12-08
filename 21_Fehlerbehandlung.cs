39e6ffa23f847b4033cb47786313a7e7249ee04b
Estragon <124503629+CodingByEstragon@users.noreply.github.com>
2023-12-08 19:17:40 +0100

    21_Fehlerbehandlung.cs

--- 21_Fehlerbehandlung.cs
+++ 21_Fehlerbehandlung.cs
@@ -4,7 +4,26 @@ namespace Ablauf
     {
         static void Main()
         {
+            // Fehlerbehandlung
+            try
+            {
+                Console.WriteLine("Gib die erste Zahl ein: ");
+                int dividend = Convert.ToInt32(Console.ReadLine());
 
+                Console.WriteLine("Gib die zweite Zahl ein: ");
+                int divisor = Convert.ToInt32(Console.ReadLine());
+
+                int result = dividend / divisor;
+                Console.WriteLine("Ergebnis der Division: " + result);
+            } 
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Fehler: Division durch Null ist nicht erlaubt. Details: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Das Programm wurde ausgeführt");
+            }
         }
     }
 }
